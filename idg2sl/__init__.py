f1cda2dbece9b4875433ea90c169eae8f4e151e1
Justin Reese <justaddcoffee+github@gmail.com>
2020-02-06 16:50:15 -0800

    Moved yeast parsing into idg2sl/idg2sl_parser_yeast.py

--- idg2sl/__init__.py
+++ idg2sl/__init__.py
@@ -1,6 +1,7 @@
 from .synthetic_lethal_interaction import SyntheticLethalInteraction
 from .entrez_parser import EntrezParser
 from .idg2sl_parser_human import *
+from .idg2sl_parser_yeast import *
 
 
 __all__ = ["SyntheticLethalInteraction", "EntrezParser"]
