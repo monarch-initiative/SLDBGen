d7befd011a49fe76c3f00a0330afce8a683e97e7
Peter Robinson <peter.robinson@jax.org>
2020-02-08 13:37:57 -0500

    Adding data for Steckel 2012

--- idg2sl/idg2sl_parser_human.py
+++ idg2sl/idg2sl_parser_human.py
@@ -5,6 +5,11 @@ from idg2sl import SyntheticLethalInteraction
 
 
 
+
+## Some constants
+activating_mutation = 'activating_mutation'
+
+
 class GenePair:
     """
     We need a class to act as a key in dictionaries with the key being made of gene A and gene B
@@ -63,7 +68,7 @@ def parse_luo2009_supplemental_file_S3(path, symbol2entrezID):
     # because of the experiment, geneA is always KRAS. GeneB is in Table S3
     kras_symbol = 'KRAS'
     kras_id = 'NCBIGene:3845'
-    kras_perturbation = 'oncogenic_mutation'
+    kras_perturbation = activating_mutation
     gene2_perturbation = 'shRNA'
     pmid = 'PMID:19490893'
     assays = ['competitive hybridization', 'multicolor competition assay']
@@ -258,6 +263,85 @@ def parse_turner_2008(path, symbol2entrezID):
                                              pmid=pmid,
                                              SL=SL)
             gene_pair = GenePair(parp1_symbol, geneB_sym)
+            sli_dict[gene_pair].append(sli)
+        sli_list = mark_maximum_entries(sli_dict)
+        return sli_list
+
+
+def parse_steckel_2012(path, symbol2entrezID):
+    """
+    Steckel M, et al. Determination of synthetic lethal interactions in KRAS oncogene-dependent cancer cells reveals novel
+    therapeutic targeting strategies. Cell Res. 2012 Aug;22(8):1227-45.  PubMed PMID: 22613949
+    HCT-116 human colon cancer cells and the isogenic derivative, HKE-3, in which the activated, but not the
+    normal, KRAS allele has been removed by homologous recombination
+    a Delta-Z-score cut-off value of 3.3 was selected to generate a primary hit list of 89 genes
+    (∼ 1.2% of the total number of genes screened)
+    KRAS itself was placed ninth in this ΔZ-score ranking list, scoring very highly in HCT-116 and weakly in HKE-3 cells
+    and thereby serving as an important internal control (Supplementary information, Table S1). Of the remaining 88 genes,
+    18 with a Z-score in excess of 2.0 in the HKE-3 cell line alone were eliminated from further analysis, following the
+    reasoning that high levels of apoptosis resulting from siRNA-mediated silencing of these genes would likely
+    constitute an undesirably strong cytotoxic effect in wild-type KRAS cells (Supplementary information, Figure S2B
+    and S2C). Thus, our starting list for further validation comprised 70 candidate genes.
+    6159 lines in Harnessing Supp.
+    Parse strategy
+    delta_zscore >= 3.3 reveals 89 candidates (correct)
+    if we remove kras and genes with HKE3_zscore > 2, we get to 70 genes (correct)
+    This does not match with the Harnessing paper, which reports 80 SL genes. We will stick with the results
+    of the initial screen, i.e., 70 genes, and count the rest as negatives.
+    """
+    kras_symbol = 'KRAS'
+    kras_id = 'NCBIGene:3845'
+    kras_perturbation = activating_mutation
+    gene2_perturbation = 'siRNA'
+    pmid = 'PMID:22613949'
+    assay_string = "RNA-interference  assay"
+    effect_type = 'stddev'
+    cell_line = 'HCT-116'
+    cellosaurus = 'CVCL_0291'
+    cancer = "Colorectal Carcinoma"
+    ncit = "NCIT:C2955"
+    if not os.path.exists(path):
+        raise ValueError("Must path a valid path for Turner et al 2008")
+    sli_dict = defaultdict(list)
+    # GeneID	Locus.ID	Accession	HCT-116 Z-score	HKE-3 Z-score	D Z-score
+    with open(path) as f:
+        next(f) # skip header
+        for line in f:
+            F = line.rstrip('\n').split('\t')
+            if len(F) != 6:
+                raise ValueError("Line has %d fields (should have 6): %s" % (len(F), line))
+            geneB_sym = F[0]
+            locusID = F[1]
+            accession = F[2]
+            HCT116_zscore = float(F[3])
+            HKE3_zscore = float(F[4])
+            delta_zscore = float(F[5])
+            if geneB_sym in symbol2entrezID:
+                geneB_id = "NCBIGene:{}".format(symbol2entrezID.get(geneB_sym))
+            else:
+                geneB_id = "n/a"
+            if geneB_sym == "KRAS":
+                continue # This was an internal control!
+            if delta_zscore >= 3.3 and HKE3_zscore < 2:
+                SL = True
+            else:
+                SL = False
+            sli = SyntheticLethalInteraction(gene_A_symbol=kras_symbol,
+                                             gene_A_id=kras_id,
+                                             gene_B_symbol=geneB_sym,
+                                             gene_B_id=geneB_id,
+                                             gene_A_pert=kras_perturbation,
+                                             gene_B_pert=gene2_perturbation,
+                                             effect_type=effect_type,
+                                             effect_size=HCT116_zscore,
+                                             cell_line=cell_line,
+                                             cellosaurus_id=cellosaurus,
+                                             cancer_type=cancer,
+                                             ncit_id=ncit,
+                                             assay=assay_string,
+                                             pmid=pmid,
+                                             SL=SL)
+            gene_pair = GenePair(kras_symbol, geneB_sym)
             sli_dict[gene_pair].append(sli)
         sli_list = mark_maximum_entries(sli_dict)
         return sli_list
