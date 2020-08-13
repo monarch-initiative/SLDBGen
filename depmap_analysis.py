837e5ac730bc2f55c04f75ed4114415bf8329a5a
Leo <leonard.spranger@googlemail.com>
2020-08-13 18:54:13 +0200

    second results

--- depmap_analysis.py
+++ depmap_analysis.py
@@ -12,6 +12,7 @@ import gzip
 
 import time
 
+genes = []
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='Process the DepMap data by downloading the file, '
@@ -31,7 +32,7 @@ def arg_parser():
 def get_data(gene_effect_threshold, cell_proportion_threshold):
     gene_effect_threshold = gene_effect_threshold
     cell_proportion_threshold = cell_proportion_threshold
-    genes = []
+    global genes
 
     print("Reducing the original data...")
     url = 'https://ndownloader.figshare.com/files/14221385'
@@ -64,41 +65,65 @@ def get_data(gene_effect_threshold, cell_proportion_threshold):
 
     i = 0
     while i in range(ensembl_df.shape[1]):
-        if ensembl_df.shape[1] % 1000 == 0:
-            print(f"Data shortened to {ensembl_df.shape[1]} genes!")
+        if i % 1000 == 0:
+            print(f"Data at {i} genes!")
         col = ensembl_df.iloc[:, i]  # corresponds to a column (which shows one gene)
         effective_cells = col.index[col < gene_effect_threshold].tolist()
         proportion = len(effective_cells) / ensembl_df.shape[0]
+        i += 1
         if not col.name.startswith("ENS"):
-            ensembl_df = ensembl_df.drop(col.name, 1)
             continue
         if not (0 < proportion < cell_proportion_threshold):
-            ensembl_df = ensembl_df.drop(col.name, 1)
             continue
-        i += 1
         gene = Gene(gene_id=col.name, effective_cells=effective_cells)
         genes.append(gene)
 
-    #ensembl_df.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'ensembl_df.csv'), index=False)
+    # ensembl_df.to_csv(os.path.join(os.path.dirname(__file__), 'data', 'ensembl_df.csv'), index=False)
     print("Calculation done!")
-    return ensembl_df, genes
+    # return genes
 
 
 def intersection(lst1, lst2):
     return [value for value in lst1 if value in set(lst2)]
 
 
-def f(id, effective_cells, genes, intersect_threshold):
+def f(i, genes, intersect_threshold):
     gene_list = []
-    for gene_B in genes:
-        if id == gene_B.get_id():
-            continue
-        cells_covered = len(set(effective_cells + gene_B.get_effective_cells()))
-        intersect = len(intersection(effective_cells, gene_B.get_effective_cells()))
+    gene_A = genes[i]
+    j = i+1
+    while j in range(len(genes)):
+        gene_B = genes[j]
+        cells_covered = len(set(gene_A.get_effective_cells() + gene_B.get_effective_cells()))
+        intersect = len(intersection(gene_A.get_effective_cells(), gene_B.get_effective_cells()))
         if intersect / cells_covered > intersect_threshold:
-            sli = SyntheticLethalInteraction(gene_A_id=id, gene_B_id=gene_B.get_id(), SL=1)
+            sli = SyntheticLethalInteraction(gene_A_id=gene_A.get_id(), gene_B_id=gene_B.get_id(), SL=1)
             gene_list.append(sli)
+        j += 1
     return gene_list
+
+
+def get_SLI(intersect_threshold):
+    results = []
+    sls = []
+    p = mp.Pool()
+    for i in range(len(genes)):
+        results.append(p.starmap_async(f, [(i, genes, intersect_threshold)]))
+
+    #results.append(f(gene.get_id(), gene.get_effective_cells(), genes, intersect_threshold))
+    #genes.remove(gene)
+    p.close()
+    print("Pools are closed!")
+    p.join()
+    print("Pool joining done")
+    print(type(results))
+    print(results[0])
+    for res in results:
+        for list in res.get():
+            for sli in list:
+                dict = {"gene_A": sli.get_gene_A_id(), "gene_B": sli.get_gene_B_id(), "SL": sli.get_SL()}
+                sls.append(dict)
+    print("sls erstellt")
+    return sls
 
 
 if __name__ == '__main__':
@@ -113,75 +138,13 @@ if __name__ == '__main__':
           f"necessary proportion of effective cells lines to be considered: {args.prop}\n"
           f"minimum intersect between the set of effective cells from two genes: {args.intersect}\n")
 
-    effective_cells_per_gene = defaultdict(str)
-    number_effective_cells_per_gene = defaultdict(int)
-    sls = []  # list containing final SL pairs
-    genes = []  # safes all genes with class entries
-
-    ensembl_df, genes = get_data(gene_effect_threshold, cell_proportion_threshold)
-    print(ensembl_df.head())
+    get_data(gene_effect_threshold, cell_proportion_threshold)
     print("starting the big boys now!")
 
-    results = []
-    p = mp.Pool()
-    i = 0
-    for gene in genes:
-        if i % 100 == 0:
-            print(i)
-        i += 1
-        # results.append(p.apply_async(f, (gene.get_id(), gene.get_effective_cells(), genes)))
-        results.append(p.starmap_async(f, [(gene.get_id(), gene.get_effective_cells(), genes, intersect_threshold)]))
-    p.close()
-    print("Pool closing done")
-    p.join()
-    print("Pool joining done")
-    for res in results:
-        for list in res.get():
-            for sli in list:
-                dict = {"gene_A": sli.get_gene_A_id(), "gene_B": sli.get_gene_B_id(), "SL": sli.get_SL()}
-                sls.append(dict)
-    print("sls erstellt")
-
-
+    sls = get_SLI(intersect_threshold)
 
     sl_pairs = pd.DataFrame(sls)
     print(sl_pairs.head())
-    #sl_pairs.to_csv('output.csv', index=False)
+    #sl_pairs.to_csv(f'output_{args.effect}_{args.prop}_{args.intersect}.csv', index=False)
 
     print(f"\nCalculation took {time.process_time() - start} seconds!\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- parse_STRING.py
+++ parse_STRING.py
@@ -2,20 +2,40 @@ import gzip
 import os
 import wget
 from collections import defaultdict
-from utils.lookup import Lookup
+# from utils.lookup import Lookup
 import idg2sl
 
-protID2ensembl = Lookup().protein2ensembl
-#print(protID2ensembl)
-#protID2ensembl_backup = EnsemblLookup().backup_protein_lookup
-protID2ensembl_backup = defaultdict(str)
-#print(protID2ensembl_backup)
-
-stringdata = os.path.join(os.path.dirname(__file__), 'data', 'STRING_9606.protein.links.v11.0.txt.gz')
-outfile = "STRING_graph_ensembl.txt"
 
 
-def parse_STRING(stringdata, outfile):
+
+def load_lookup():
+    protein2ensembl = defaultdict(str)
+    with gzip.open(os.path.join(os.path.dirname(__file__), 'lookup', 'gene2ensembl.gz'), 'rt') as f:
+        next(f)  # skip header
+        for line in f:
+            fields = line.split('\t')
+            if fields[0].strip() in ["9606"]:
+                ensembl = fields[2]
+                if fields[6].startswith("E") and fields[6] is not None:
+                    prot_ID = fields[6].split(".")[0]
+                    protein2ensembl[prot_ID] = ensembl
+    return protein2ensembl
+
+
+def load_backup():
+    protein2ensembl = defaultdict(str)
+    with gzip.open(os.path.join(os.path.dirname(__file__), 'lookup', 'prot2gene.gz'), 'rt') as f:
+        next(f)  # skip header
+        for line in f:
+            fields = line.split('\t')
+            ensembl = fields[0]
+            if fields[1].startswith("E") and fields[1] is not None:
+                prot_ID = fields[1]
+                protein2ensembl[prot_ID] = ensembl
+    return protein2ensembl
+
+
+def parse_STRING(stringdata, cutoff, outfile, protID2ensembl):
     found = 0
     nfound = 0
     with gzip.open(stringdata, 'rt') as f:
@@ -23,30 +43,39 @@ def parse_STRING(stringdata, outfile):
         with open(outfile, 'w') as out_f:
             for line in f:
                 fields = line.split(' ')
+                if int(fields[2]) <= cutoff:
+                    continue
                 protA = fields[0].split(".")[1]
                 protB = fields[1].split(".")[1]
-                ensemblA, ensemblB = "", ""
                 if protA in protID2ensembl:
                     ensemblA = protID2ensembl.get(protA)
                     found += 1
-                elif protA in protID2ensembl_backup:
-                    ensemblA = protID2ensembl.get(protA)
-                    found += 1
+                #elif protA in protID2ensembl_backup:
+                #    ensemblA = protID2ensembl_backup.get(protA)
+                #    found += 1
                 else:
-                    ensemblA = "n/a"
                     nfound += 1
+                    continue
                 if protB in protID2ensembl:
                     ensemblB = protID2ensembl.get(protB)
                     found += 1
-                elif protB in protID2ensembl_backup:
-                    ensemblB = protID2ensembl.get(protB)
-                    found += 1
+                #elif protB in protID2ensembl_backup:
+                #    ensemblB = protID2ensembl_backup.get(protB)
+                #    found += 1
                 else:
-                    ensemblB = "n/a"
                     nfound += 1
+                    continue
                 out_f.write("STRING_data\t" + ensemblA + "\t" + ensemblB + "\t" + fields[2])
-    print("Found %d proteins, didn't find %d proteins" % (found, nfound))
-    print(nfound/(found + nfound))
+    print(f"Found {found} proteins, didn't find {nfound} proteins")
+    print(f"{round(nfound / (found + nfound), 2)}% couldn't be found!")
 
 
-parse_STRING(stringdata, outfile)
+if __name__ == "__main__":
+    stringdata = os.path.join(os.path.dirname(__file__), 'data', 'STRING_9606.protein.links.v11.0.txt.gz')
+    outfile = "STRING_graph_ensembl.txt"
+    cutoff = 700
+
+    lookup = load_lookup()      # maps ~93.2% of the IDs
+    backup = load_backup()      # maps ~97.9% of the IDs
+    # when using both files we also get ~97.9% mapping
+    parse_STRING(stringdata, cutoff, outfile, backup)
