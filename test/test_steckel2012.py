ce67e51ac74a3ac699a80fea962f027ddfed8e10
Peter Robinson <peter.robinson@jax.org>
2021-02-20 11:47:08 -0500

    fixing unit tests

--- test/test_steckel2012.py
+++ test/test_steckel2012.py
@@ -37,7 +37,7 @@ class TestSteckel2012(TestCase):
         self.assertEqual("POLR2A", self.first_entry.get_gene_B_symbol())
 
     def test_get_perturbation(self):
-        self.assertEqual("activating_mutation", self.first_entry.get_gene_A_pert())
+        self.assertEqual("activating mutation", self.first_entry.get_gene_A_pert())
         self.assertEqual("siRNA", self.first_entry.get_gene_B_pert())
 
     def test_get_cellosaurus(self):
